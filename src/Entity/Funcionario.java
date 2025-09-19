e472e5fbbb652b638a87f164ea5d6638510f5f82
Fernanda Xabude <fernandabomfilioli@gmail.com>
2025-09-19 20:11:19 -0300

    Resolução de conflito

--- src/Entity/Funcionario.java
+++ src/Entity/Funcionario.java
@@ -5,11 +5,13 @@ import java.time.LocalDate;
 public class Funcionario extends Pessoa {
 
 	private double salarioBruto;
+	private double descontoInss;
+	private double descontoIR;
 
 	public Funcionario() {
 		super();
 	}
-	
+
 	public Funcionario(String nome, String cpf, LocalDate dataDeNascimento, double salarioBruto) {
 		super(nome, cpf, dataDeNascimento);
 		this.salarioBruto = salarioBruto;
@@ -23,13 +25,27 @@ public class Funcionario extends Pessoa {
 		this.salarioBruto = salarioBruto;
 	}
 
+	public double getDescontoInss() {
+		return descontoInss;
+	}
+
+	public void setDescontoInss(double descontoInss) {
+		this.descontoInss = descontoInss;
+	}
+
+	public double getDescontoIR() {
+		return descontoIR;
+	}
+
+	public void setDescontoIR(double descontoIR) {
+		this.descontoIR = descontoIR;
+	}
+
 	@Override
 	public String toString() {
-		return "\nFuncionario:\nNome: " + getNome() + " CPF: " + getCpf() 
-				+ " Data de nascimento: "  + getDataDeNascimento() + " Salario bruto: " + getSalarioBruto() + "\nDependentes:";
+		return "\nFuncionario:\nNome: " + getNome() + " CPF: " + getCpf() + " Data de nascimento: "
+				+ getDataDeNascimento() + " Salario bruto: " + getSalarioBruto() + "\nDependentes:";
 
 	}
-	
-	
 
 }
